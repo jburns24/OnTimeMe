c7a5f9a6a7247f5c41bb96b69ac064413e29d580
avue7 <avue7@mail.csuchico.edu>
2018-03-04 02:54:20 -0800

    Added the methods from login-gate to this file for the google plus login. Had to migrate the methods and properties here to make it global so that initializeApp method can perform a check on users logged in or not properly. Without migrating it here, it would be disastrous to perform such logic unless we use google plus silent method.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
+/**
+ * NOTE: This is the global controller for this app. All that you want to happen
+ * globally goes here. Everything that you want to happen eventfully, goes to
+ * it's appropriate place. This is the place that will initialize or make things
+ * happen each time the app is opened or started. Be careful what you put in here.
+ */
 import { Component, ViewChild } from '@angular/core';
-import { Platform, MenuController, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 import { LoginGatePage } from '../pages/login-gate/login-gate';
 import { HomePage } from '../pages/home/home';
 import { PreferencePage } from '../pages/preference/preference';
+import { NativeStorage } from '@ionic-native/native-storage';
+import {
+  Platform,
+  MenuController,
+  Nav,
+  LoadingController,
+  Loading
+} from 'ionic-angular';
+
 
 @Component({
   templateUrl: 'app.html'
@@ -13,6 +27,7 @@ import { PreferencePage } from '../pages/preference/preference';
 export class MyApp {
   pages;
   rootPage;
+  loading: Loading;
 
   // For use with login and logout (logic implementation)
   @ViewChild(Nav) nav: Nav;
@@ -21,15 +36,19 @@ export class MyApp {
     private platform: Platform,
     private statusBar: StatusBar,
     private splashScreen: SplashScreen,
-    private menu: MenuController
+    private menu: MenuController,
+    private loadingCtrl: LoadingController,
+    private storage: NativeStorage,
+    private alertCrl: AlertController
   ){
-    // This function will initialize the app upon opening app
+    // This function will initialize the app upon opening the app.
+    // Anything you want initialized, do it here!!!!
     this.initializeApp();
 
     // Set our app's pages in the left menu; Add new pages here!!!
     this.pages = [
       { title: 'Home', component: HomePage, icon:'home' },
-      { title: 'Preference', component: PreferencePage }
+      { title: 'Preference', component: PreferencePage, icon:'md-settings'}
     ];
 
     // Set the app to land on the login page as soon as launch
@@ -37,28 +56,51 @@ export class MyApp {
   }
 
   initializeApp(){
-    this.platform.ready().then(() => {
+    this.platform.ready()
+    .then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
 
-    /* Adding logic to check if user is logged in or not.
-    this.auth.afAuth.authState.subscribe(
-      user => {
-        if (user) {
-          this.rootPage = HomePage;
-        }
-        else {
-          this.rootPage = LoginGatePage;
-        }
-      },
-      () => {
-        this.rootPage = LoginGatePage;
-      }
-    );*/
+      /*** This is where the logic is implemented for checking user log ins ***/
+      this.storage.getItem('user') // Try to get item from local storage and...
+      .then( (data) => {
+        // Succeed, profile exists...allow that person to access his/her data.
+        this.nav.setRoot(HomePage);
+        this.splashScreen.hide();
+      }, (error) => {
+        // Failed, user not logged on, ask him/her to log in
+        this.nav.setRoot(LoginGatePage);
+        this.splashScreen.hide();
+      });
+    }, (err) => {
+        console.log(err);
+    });
   }
+
+  showLoading() {
+    this.loading = this.loadingCtrl.create({
+      content: 'Please wait...',
+      dismissOnPageChange: true
+    });
+    this.loading.present();
+  }
+
+  dismissLoading() {
+    this.loading.dismiss();
+  }
+
+  showError(text) {
+    this.loading.dismiss();
+
+    let alert = this.alertCrl.create({
+      title: 'Fail',
+      subTitle: text,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
   // Need this for menu to work
   openPage(page){
     this.menu.close();
