5e3446141411e3222e771fb0a852ec01720ab123
avue7 <avue7@mail.csuchico.edu>
2018-03-06 03:27:29 -0800

    migrated the logout button to the side menu. Also, fixed the styling on the avatar to get rid of that huge space. Put the users email right on the side of the avatar.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, MenuController } from 'ionic-angular';
-import { GooglePlus } from '@ionic-native/google-plus';
-import { LoginGatePage } from '../login-gate/login-gate';
+//import { GooglePlus } from '@ionic-native/google-plus';
+//import { LoginGatePage } from '../login-gate/login-gate';
 import { NativeStorage } from '@ionic-native/native-storage';
 
 @Component({
@@ -9,24 +9,26 @@ import { NativeStorage } from '@ionic-native/native-storage';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  user: any;
+  user: any = null;
   email: any;
   picture: any;
 
-  constructor(public navCtrl: NavController, private googlePlus: GooglePlus,
-    private menu: MenuController, private storage: NativeStorage){
-    // After we login and land on the home page, enable the menu for current user
-    this.enableMenu();
-    this.getUser();
+  constructor(
+    public navCtrl: NavController, /*private googlePlus: GooglePlus,*/
+    private menu: MenuController,
+    private storage: NativeStorage){
+      // After we login and land on the home page, enable the menu for
+      // current user
+      this.enableMenu();
+      this.getUserInfo();
   }
 
   enableMenu(){
     this.menu.enable(true);
   }
 
-  getUser(){
-    this.storage.getItem('user')
-    .then((data) => {
+  getUserInfo(){
+    this.storage.getItem('user').then((data) => {
       this.user = data.name;
       this.email = data.email;
       this.picture = data.picture;
@@ -36,21 +38,20 @@ export class HomePage {
     });
   }
 
-  // When user logs-out, we must delete the users local profile, to
+  ///////////// NOTE: KEEEEEEEEEPPPPPPPPPPPPP MEEEEEEEEEEEEEEEEE!!!! ///////
+  /*// When user logs-out, we must delete the users local profile, to
   // allow our app to know that no user's are logged on. The check for
   // users logged on? is in the app.component.ts file.
   logout () {
     this.googlePlus.trySilentLogin({
       'webClientId': '311811472759-j2p0u79sv24d7dgmr1er559cif0m7k1j.apps.googleusercontent.com'
-    })
-    .then ((res) => {
+    }).then ((res) => {
       this.googlePlus.logout().then((response) => {
         this.storage.remove('user');
         this.navCtrl.setRoot(LoginGatePage);
         console.log("successful logout");
       }, (error) => {
-        this.googlePlus.disconnect()
-        .then ((res) => {
+        this.googlePlus.disconnect().then ((res) => {
           this.storage.remove('user');
           this.navCtrl.setRoot(LoginGatePage);
           console.log("Successfully disconnected");
@@ -62,5 +63,6 @@ export class HomePage {
     }, (error) => {
       console.log ("trySilentLogin failed", error);
     });
-  }
+  }*/
+  /////////////////////////////////////////////////////////////////////////////
 }
