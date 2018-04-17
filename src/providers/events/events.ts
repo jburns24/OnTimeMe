f5aa9f7618d67586fe348db7ca932e0a16539345
Athit Vue <avue7@mail.csuchico.edu>
2018-04-16 17:01:16 -0700

    Fixed the logic for the events provider so that the promise in getTodaysEvents method will resolve in the correct # of events. The double for loops in this method was causing an async problem that requires synchronization due to the dependency that the first loop must finish executing before the second loop can be executed correctly.

--- src/providers/events/events.ts
+++ src/providers/events/events.ts
@@ -77,10 +77,12 @@ export class Events {
       this.user.getUserInfo().then((user) => {
         this.storage.getItem(user.id + 'events').then((eventList) => {
           let events = eventList['eventList'];
-          console.log("got the event list!!", events);
           this.map.getPreferenceMode().then((mode) => {
             this.mode = mode;
+            let counter = 0;
+            console.log("event list length is:", events);
             for (let event of events) {
+              counter = counter + 1;
               let ongoing = 0;
               // Skipping any event that does not have a location.
               if(typeof event['location'] === 'undefined') {
@@ -98,48 +100,12 @@ export class Events {
                 happening: ongoing,
               };
               this.modEventList.push(new_event);
-            }
-            console.log("modified event list is ", this.modEventList);
-            for (let eventd of this.modEventList) {
-              let origin = this.locationTracker.lat + ',' + this.locationTracker.lng;
-              this.map.getDistance(eventd['location'], mode, origin).then ((suc) => {
-                // Recreate event list with trip duration
-                let rows = suc['rows'];
-                let row = rows[0];
-                let elements = row['elements'];
-                let element = elements[0];
-                let duration = element['duration'];
-                let event_with_trip = {
-                  id: eventd['id'],
-                  summary: eventd['summary'],
-                  location: eventd['location'],
-                  startTime: eventd['startTime'],
-                  endTime: eventd['endTime'],
-                  happening: eventd['happening'],
-                  trip_duration: duration['value'],
-                };
-                this.eventListWithTrip.push(event_with_trip);
-                this.eventListWithTrip.sort((a:any, b:any) => {
-                  if(a.startTime < b.startTime) {
-                    return -1;
-                  }
-                  else if(a.startTime > b.startTime) {
-                    return 1;
-                  }
-                  else {
-                    return 0;
-                  }
+              if (counter === events.length){
+                this.addTripToList(this.modEventList, mode).then((eventsWithTrip) => {
+                  resolve(eventsWithTrip);
                 });
-                // Store for offline use...add to this later on....
-                this.storage.setItem('lastKnownLocation', {origin: suc['origin_addresses']});
-              }, (error) => {
-                console.log('Events::getTodaysEvents(): failed to get distance:', error);
-              });
-            }
-            // Storing eventListWithTrip as a JSON String.
-            this.storage.setItem('eventWithTrip', JSON.stringify(this.eventListWithTrip)).then(() => {
-              resolve(this.eventListWithTrip);
-            });
+              };
+            };
           },(err) =>{
             console.log('Events::getTodaysEvents: failed to get prefrence mode', err);
           });
@@ -149,6 +115,63 @@ export class Events {
       }, (err) => {
         console.log('Events::getTodaysEvents(): failed to fetch events ', err);
       });
+    });
+  }
+
+  addTripToList(modEventList: any, mode: any){
+    return new Promise(resolve => {
+      console.log("modified event list is ", this.modEventList);
+      let counter = 0;
+      for (let eventd of modEventList) {
+        counter = counter + 1;
+        let origin = this.locationTracker.lat + ',' + this.locationTracker.lng;
+        this.map.getDistance(eventd['location'], mode, origin).then ((suc) => {
+          // Recreate event list with trip duration
+          let rows = suc['rows'];
+          let row = rows[0];
+          let elements = row['elements'];
+          let element = elements[0];
+          let duration = element['duration'];
+          let event_with_trip = {
+            id: eventd['id'],
+            summary: eventd['summary'],
+            location: eventd['location'],
+            startTime: eventd['startTime'],
+            endTime: eventd['endTime'],
+            happening: eventd['happening'],
+            trip_duration: duration['value'],
+          };
+          this.eventListWithTrip.push(event_with_trip);
+          this.eventListWithTrip.sort((a:any, b:any) => {
+            if(a.startTime < b.startTime) {
+              return -1;
+            }
+            else if(a.startTime > b.startTime) {
+              return 1;
+            }
+            else {
+              return 0;
+            }
+          });
+          // Store for offline use...add to this later on....
+          this.storage.setItem('lastKnownLocation', {origin: suc['origin_addresses']});
+
+          // Check if for loop is done; if done, call to resolve
+          console.log("Events::getTodaysEvents(): modEventList length is:", modEventList.length);
+          if (counter === modEventList.length){
+            this.storage.setItem('eventWithTrip', JSON.stringify(this.eventListWithTrip));
+            resolve(this.forLoopIsFinished(this.eventListWithTrip));
+          };
+        }, (error) => {
+          console.log('Events::getTodaysEvents(): failed to get distance:', error);
+        });
+      };
+    });
+  }
+
+  forLoopIsFinished(events: any){
+    return new Promise(resolve => {
+      resolve(events);
     });
   }
 }
